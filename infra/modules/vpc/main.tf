58c573b71daddc6a98e3607546318fd981f87ca6
adeoluadeyemi <adeolu.ooa@gmail.com>
2024-09-23 15:23:14 +0100

    Remove aws key pair

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -149,7 +149,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "capstone-key"
-  public_key = file("/home/adeolu/.ssh/capstone_key.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "capstone-key"
+#   public_key = file("/home/adeolu/.ssh/capstone_key.pub")
+# }
