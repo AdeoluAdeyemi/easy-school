fff097f6c862af1de830f25a3d1d92ef78861be2
adeoluadeyemi <adeolu.ooa@gmail.com>
2024-09-23 18:23:14 +0100

    Updated security group

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -107,7 +107,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "capstone_allow_http_ipv4" {
   security_group_id = aws_security_group.capstone_allow_http.id
-  cidr_ipv4         = aws_vpc.capstone_vpc_main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -115,7 +115,7 @@ resource "aws_vpc_security_group_ingress_rule" "capstone_allow_http_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.capstone_vpc_main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
